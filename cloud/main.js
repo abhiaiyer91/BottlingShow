ccffea909162bb9c4d6d2eff9f463d0132a1eacc
abhiaiyer91 <abhiaiyer91@gmail.com>
2014-07-18 13:26:32 -0700

    MahaskaShowOff

---
+++ cloud/main.js
@@ -0,0 +1,31 @@
+
+/* global Parse,console,require */
+
+var Mailgun = require('mailgun');
+Mailgun.initialize('sandbox97f57fe5c9d14f50b5c4521c43285282.mailgun.org', 'key-0723-mbxn3dxzmowc-mgthmbov4kjzn5');
+
+Parse.Cloud.beforeSave("Email", function(request, response) {
+
+	var text = "Email from Mahaska.com contact form\n" +
+		"From: "+request.object.get("name") + "\n\n"+
+		"Email: "+request.object.get("email") + "\n\n"+
+    "Phone: "+request.object.get("phone") + "\n\n"+
+		"Reason: "+request.object.get("reason") + "\n\n"+
+		"Message:\n\n" + request.object.get("message");
+
+	Mailgun.sendEmail({
+			to: "abhi@anthro.is, ryan@anthro.is",
+			from: request.object.get("email"),
+			subject: "Email For - " + request.object.get("reason"),
+			text: text
+		}, {
+		success: function(httpResponse) {
+			response.success();
+		},
+		error: function(httpResponse) {
+			console.error(httpResponse);
+			response.error("Uh oh, something went wrong");
+		}
+	});
+
+});
